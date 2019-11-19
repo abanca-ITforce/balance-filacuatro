767d1c272f4357e1b6f572d9fc7daaf864a37af5
Cris Pampin <cristina.rpampin@gmail.com>
2019-11-19 22:50:37 +0100

    fix all

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home/home.component';
+import { ShellComponent } from './layout/shell/shell.component';
 
 
 const routes: Routes = [] = [
-  { path: "", component: HomeComponent },
+  { path: "", component: ShellComponent },
 ];
 
 @NgModule({
