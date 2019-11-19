02c3146713d28b3d59870ed36c340ec8987db01a
AdrianArnoso <57632143+AdrianArnoso@users.noreply.github.com>
2019-11-19 20:43:12 +0100

    tenemos un problema al hacer deploy: loaf children null

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -4,6 +4,7 @@ import { ShellComponent } from './shell/shell.component';
 import { TopBarComponent } from './shell/top-bar/top-bar.component';
 import { MainContentComponent } from './shell/main-content/main-content.component';
 import { CopyrightComponent } from './shell/copyright/copyright.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -11,7 +12,8 @@ import { CopyrightComponent } from './shell/copyright/copyright.component';
 @NgModule({
   declarations: [ShellComponent, TopBarComponent, MainContentComponent, CopyrightComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     ShellComponent
